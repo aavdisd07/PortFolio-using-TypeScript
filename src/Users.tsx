756813ee08b6cebaa9a85dc76d71a2e07641317a
Avantika Deshmukh <2021bit153@sggs.ac.in>
2024-08-04 23:26:23 +0530

    check changes

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,5 +1,5 @@
 const Info = {
-  stack: ["Fronted Developer", "Competitive Programmer"],
+  stack: ["Fronted Developer", "UX/UI Designer",""],
   bio: " Student of Information Technology with a strong passion for frontend development and UI/UX design. With hands-on experience in creating responsive websites and applications using HTML, CSS, JavaScript, React, and Flutter, I am proficient in delivering user-friendly and visually appealing digital solutions",
 };
 export { Info };
