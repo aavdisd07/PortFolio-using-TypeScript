b0bfc6ff3470115b6d478f440919bbf001297744
Avantika Deshmukh <2021bit153@sggs.ac.in>
2024-08-09 15:55:02 +0530

    resumeviewer edited

--- src/Components/ResumeViewer.tsx
+++ src/Components/ResumeViewer.tsx
@@ -19,7 +19,7 @@ const ResumeViewer = (props: any) => {
           <Modal.Title data-autofocus className="!text-4xl xs-mx:!text-2xl text-white flex gap-3 items-center !font-bold">
             Resume
             <Tooltip label="Download" className="!text-bgColor" color="#64FFDA" position="right" offset={5}>
-              <ActionIcon className="!text-primaryColor" component="a" href="/Avantika_Resume.pdf" size={btn} download="AvantikaDeshmukhResume" variant="outline" color="#64FFDA">
+              <ActionIcon className="!text-primaryColor" component="a" href="Avantika_Resume.pdf" size={btn} download="AvantikaDeshmukhResume" variant="outline" color="#64FFDA">
                 <IconArrowBigDownLineFilled className="xs-mx:!w-[16px]" />
               </ActionIcon>
             </Tooltip>
@@ -27,7 +27,7 @@ const ResumeViewer = (props: any) => {
           <Modal.CloseButton size="md" iconSize="30px" className="!bg-bgColor !text-red-500" />
         </Modal.Header>
         <Modal.Body className="!bg-bgColor xs-mx:!p-2 !pt-2 !border-primaryColor !border-2 xs-mx:!border xs-mx:!border-t-0 !border-t-0 !rounded-bl-3xl !rounded-br-3xl">
-          <Document className="w-full !rounded-2xl !overflow-hidden !min-w-40 !min-h-14" file="/Avantika_Resume.pdf">
+          <Document className="w-full !rounded-2xl !overflow-hidden !min-w-40 !min-h-14" file="Avantika_Resume.pdf">
             <Page className="w-full !min-w-40 !min-h-14 md-mx:[&>.react-pdf\\_Page\\canvas]:!w-full md-mx:[&>.react-pdf\\Page\\_canvas]:!h-auto" pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
           </Document>
         </Modal.Body>
