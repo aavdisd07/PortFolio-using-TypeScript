752a6bf91972e1f7cf2bcc7f93bf1891adbe205a
Avantika Deshmukh <2021bit153@sggs.ac.in>
2024-08-09 15:02:46 +0530

    app.tsx modify

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import HomePage from './Components/HomePage';
 import { pdfjs } from 'react-pdf';
 
 
-
-
-// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-//   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.9.179/pdf.worker.min.js',
-//   import.meta.url,
-// ).toString();
-
 pdfjs.GlobalWorkerOptions.workerSrc=new URL(
 'pdfjs-dist/build/pdf.worker.min.mjs',
 import.meta.url,
