806324f052d1fef0acacc7aa8090760dcec487aa
Avantika Deshmukh <2021bit153@sggs.ac.in>
2025-01-12 23:36:15 +0530

    Magicui updated

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,15 +1,19 @@
 import { IconHexagon } from "@tabler/icons-react";
-import IconCloud from "./magicui/icon-cloud";
 import { Slugs } from "../Users";
+import { IconCloud } from "../Components/magicui/icon-cloud";
 
 export function Loader() {
+  const images = Slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`,
+  );
+
   return (
     <div
       className="relative flex h-full w-full  items-center justify-center 
     animate-[ping_1.5s_ease-in-out_1_3.2s]
    px-20 pb-20 pt-8"
     >
-      <IconCloud iconSlugs={Slugs} />
+      <IconCloud images={images} />
       <IconHexagon
         className="absolute -z-10 animate-[spin_5s_linear_infinite]"
         size={120}
